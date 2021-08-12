b9a400e92bc96a16a5831951dca7c411dd68edb6
Joy Agustian <Pro@Joy-Agustian.local>
2021-08-12 10:58:58 +0700

    new components

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  :root{
+    --dark-bg: #ffffff;
+    //putih
+    --gray-1: #0896fb;
+    //biru kalem
+    --deep-dark: #96d3c9;
+    //teal
+    --gray-2: #66b3e4;
+    //biru pucat
+    --white : white;
+    --black: red;
+  }
+  html{
+    font-size: 10px;
+    font-family: 'Roboto Mono';
+    background-color: var(--dark-bg);
+  }
+  ul,li{
+    list-style: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  img, svg{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  button{
+    outline: none
+  }
+  .container {
+    max-width: 1200px;
+    width: 90%;
+    margin: 0 auto;
+  }
+/* Smooth Scroll  */
+  [data-scrollbar] {
+    height: 100vh;
+    overflow: hidden;
+    background-color: var(--gray-1);
+    .scroll-content {
+      background-color: var(--dark-bg);
+    }
+    .scrollbar-track.scrollbar-track-y {
+      background: var(--deep-dark);
+      .scrollbar-thumb-y {
+        background: var(--gray-1);
+      }
+    }
+  }
+`;
+export default GlobalStyles;
